66a56b314044660ab032b07596e1fc9486f5f9c6
R0BL0XIAND3M0 <55234806+DsGamer123@users.noreply.github.com>
2022-12-07 04:26:33 -0500

    Built and published by Moonwave

---
+++ assets/js/bc4ae3ae.d137b096.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[649],{26986:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"MadworkMaid","desc":"A fork of Quenty\'s Maid class, specifically designed for usage with Madwork-related tasks.","source":{"line":27,"path":"src/ReplicaService-Demo/MadworkSignal.lua"}}')}}]);
